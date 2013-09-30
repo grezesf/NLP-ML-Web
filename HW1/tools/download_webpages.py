9b76738ab8a57ab22cd738793ea0c2123c113a35
michellemorales <michellemorales@Michelles-MacBook-Pro.local>
2013-09-30 14:15:21 -0400

    Added comments to download_webpages.py

--- HW1/tools/download_webpages.py
+++ HW1/tools/download_webpages.py
@@ -3,9 +3,7 @@ import os
 import sys
 import urllib
 from urllib import *
-import bs4
-from bs4 import BeautifulSoup
-import re
+
 
 ###README
 #TODO add description here and to main README.txt
@@ -17,23 +15,22 @@ import re
 # This link connects specifically to game reviews for the iPhone/iPad
 
 
-links_file= open('/users/michellemorales/Desktop/NLP-ML-Web-master/HW1/tools/link-list.txt')
+links_file= open('link-list.txt') # Opens text file that contains list of links
 links= links_file.readlines()
-cleanLinks= [] # Contains a list of all links to metacritic
-        
+
+cleanLinks= [] # Contains a cleaned list of all links to metacritic
 for item in links:
     if item != "\n":
         cleanLinks.append(item.strip())
 
 
-for link in cleanLinks[:1]:
+for link in cleanLinks[:1]: #Iterates through list of links and prints html source code to file
     linkList= link.split('/')
-    title= linkList[-1]        
+    title= linkList[-1]   
+         
     webPage= URLopener().open(link)
     htmlDoc= webPage.read()
     webPage.close()
-    soup= BeautifulSoup(htmlDoc)
-
     outFile= open('../data/train/'+title+'.htm', 'w')
     writeToFile= outFile.write(htmlDoc)
     
