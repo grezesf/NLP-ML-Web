e173ce4fcf90d9a76ed21ef1027f5c1a7460f4a1
Felix GREZES <grezesf@gmail.com>
2013-10-01 16:58:21 -0400

    work done on gen labels

--- HW1/tools/generate_labels.py
+++ HW1/tools/generate_labels.py
@@ -4,6 +4,8 @@ import sys
 
 from bs4 import BeautifulSoup
 
+print '\n'
+
 ###README
 # this script takes a directory of html pages 
 # and outputs the proper training labels in the same dir
@@ -14,15 +16,46 @@ from bs4 import BeautifulSoup
 
 # directory containing the html pages
 htm_dir = sys.argv[1]
-print htm_dir
+print "directory containing the htm files: " + htm_dir
 
-#directory to save the labels file in (deprecated, used to be one above)
+#directory to save the labels file in (deprecated, used to be one above ie cd .. )
 # work_dir = '/'.join(htm_dir.split('/')[:-1])
 # print work_dir
 
 # name of label.txt
 labels_name = htm_dir.split('/')[-1] + "_labels.txt"
-print labels_name
+print "name of the labels file to be created: " + labels_name
+print '\n'
 
 # create and open labels file
-labels = open(htm_dir + '/' + labels_name)
+labels = open(htm_dir + '/' + labels_name, 'w+')
+
+# for all html file in the dir
+for (path, dirs, files) in os.walk(htm_dir):
+    for f in files:
+        # only work on .htm files
+        if '.htm'in f:
+            # print f
+            # make soup object
+            soup_obj = BeautifulSoup(open(path + '/' + f))
+
+            #link
+            page_link = soup_obj.find_all(rel="canonical")[0]['href'].replace('/','_')
+            print page_link
+            # MEDIA
+            media = soup_obj.find("meta")
+            # [0]['content'].upper()
+            print media
+            # GRADE
+            # OLD/NEW
+            # SCORE
+            # score = soup_obj.findAll(<span class="score_value" itemprop="ratingValue">
+            # USER-RATINGS
+
+            
+
+
+
+
+# close file
+labels.close()
