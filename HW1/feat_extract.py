43581c421c00d9c14f41ad96dd64e4187f00420d
michellemorales <michellemorales@Michelles-MacBook-Pro.local>
2013-10-02 15:07:00 -0400

    Updated feat_extract

--- HW1/feat_extract.py
+++ HW1/feat_extract.py
@@ -20,8 +20,8 @@ print "working in: " + htm_dir
 # arff file name
 arff_filename =  os.path.basename(os.path.normpath(htm_dir)) + "-feats.arff"
 
-# create and open labels file
-labels = open(htm_dir + '/' + arff_filename, 'w+')
+# create and open arff file
+arff = open(htm_dir + '/' + arff_filename, 'w+')
 print "creating: " + htm_dir + '/' + arff_filename
 
 # for all html file in the dir
@@ -32,12 +32,38 @@ for (path, dirs, files) in os.walk(htm_dir):
             print f
             # make soup object
             soup_obj = BeautifulSoup(open(path + '/' + f))
-            # extract text that we care about
 
+            # this does in fact give you the review text it just also includes, nonsense
+            # source code, perhaps the best way is just to format the output
+            text_lines = soup_obj.get_text().encode('utf-8').strip().split("\n")
+            
+
+            # iterates through the all the text, and pulls out the good stuff, ignoring nonsense
+            # i believe a good measure would be to start at 'release date' and end at 'see all user reviews'
+            start = 0
+            end = 0
+            for line in text_lines:
+                if line == "Release Date:" :
+                    index = text_lines.index(line)
+                    start += index
+                elif "See all" in line and "User Reviews" in line:
+                    index =  text_lines.index(line)
+                    end += index
+                    
+            # cleans list, removes duplicate  empty string items, and removes white space
+            clean_list = []
+            for i in list(set(text_lines[start : end + 1])):
+                clean_list.append(i.strip())
+                
+            # joins list into one string of text
+            important_text = '\n'.join(clean_list)
+            print important_text
+            
+  
             # print soup_obj.get_text().encode('utf-8').strip()
             # reviews = soup_obj.find_all(="review_body")
             # print len(reviews)
             # print reviews.get_text().encode('utf-8').strip()
 
 # close the aarf file
-labels.close()
+arff.close()
