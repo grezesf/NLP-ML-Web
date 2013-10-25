e5039f3d225b30f5fdc939db9a3ecf1da7f1b2cd
michellemorales <michellemorales@Michelles-MacBook-Pro.local>
2013-10-25 11:33:04 -0400

    updated find_dobs.py

--- HW3/tools/find_dobs.py
+++ HW3/tools/find_dobs.py
@@ -3,22 +3,26 @@ import os
 import sys
 import urllib
 from bs4 import BeautifulSoup
+import dateutil.parser as dparser
 
 ### README
 # takes a list of names and uses unstructured data to find DOBs
 
-# opens search url(yahoo)
+# opens search url(yahoo) and creates beautiful soup object
 url = 'http://search.yahoo.com/search?p=Kristina%20Sarceno'
 soup = BeautifulSoup(urllib.urlopen(url).read())
 
 
 search_links = []
 
+# searches through all a tags and finds all hrefs(links). saves link to the link list
 for tag in soup.find_all('a'):
     link = tag.get('href')
     search_links.append(link)
 
 all_text = []
+# opens every link, makes a beautiful soup object, and searches for person's name within text
+# if name in text, the text is saved
 for link in search_links[:100]:
     print 'processing link... ' + link + '\n'
     try:
@@ -32,8 +36,6 @@ for link in search_links[:100]:
         print 'Link not valid: ' + link + '\n'
 
 
-
+# searches for dates
 for text in all_text:
-    print text
-
-print len(all_text)
+    print dparser.parse(text)
