28a6572d4804871998bd2a0935aeabff51acf886
michellemorales <michellemorales@Michelles-MacBook-Pro.local>
2013-11-19 15:17:34 -0500

    ran scrape to get more data

---
+++ Project/tools/date_distribution.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+
+### README
+# The use of this script is to find the distribution of date over our data set of questions, it takes
+# a folder of files (each file is a question site html), and outputs 1 text file of a list of dates and scores
+
+folder =  os.listdir(
