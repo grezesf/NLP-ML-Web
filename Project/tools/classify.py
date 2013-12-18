14f9160d8a7d72f7674d21d10578b958d3d6a648
michellemorales <michellemorales@Michelles-MacBook-Pro.local>
2013-12-18 13:53:02 -0500

    Feat_extract_task 2 - Takes the data and extracts the revisions, and outputs a text file(pair_edits.txt) with edit pairs (deletion, followed by addition). A total of 20,096 pairs. Classify.py- takes the pair_edits.txt, and classifies spelling edits. Prints out the pre and post correction word, and then, prints out a total count (spelling corrections) and a subtotal count (capitalization corrections). The results for that can be seen in classify_results1.txt

---
+++ Project/tools/classify.py
@@ -0,0 +1,86 @@
+import re, collections, os, sys
+
+
+### README ###
+# This script classifies edits into meaning-preserving and meaning-altering
+# Input- text file (pair_edits.txt) 
+# Ouptut- text file of labeled edits
+
+
+### SPELL CHECKER ### Code borrowed from-- http://norvig.com/spell-correct.html
+def words(text): return re.findall('[a-z]+', text.lower()) 
+
+def train(features):
+    model = collections.defaultdict(lambda: 1)
+    for f in features:
+        model[f] += 1
+    return model
+
+NWORDS = train(words(file('dictionary.txt').read()))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def edits1(word):
+   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+   deletes    = [a + b[1:] for a, b in splits if b]
+   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+   inserts    = [a + c + b     for a, b in splits for c in alphabet]
+   return set(deletes + transposes + replaces + inserts)
+
+def known_edits2(word):
+    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+
+def known(words): return set(w for w in words if w in NWORDS)
+
+def correct(word):
+    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
+    return max(candidates, key=NWORDS.get)
+
+
+
+
+### CLASSIFY SPELLING CORRECTIONS ###
+# Spelling correction count and capitalization correction count
+counter = 0
+capital_correction = 0
+# Opens text file containing edits and reads them
+edit_pairs = open('../data/pair_edits.txt', 'r').readlines()
+
+for line in edit_pairs:
+
+  index = edit_pairs.index(line)
+
+  # If deletion in line then take the deletion and following addition as an edit pair
+  if 'DELETION' in line:
+    deletion = line.split(':')[-1].strip()
+    addition = edit_pairs[index+1].split(':')[-1].strip()
+    # print 'DELETION '+ deletion + ' ADDITION ' + addition
+
+    # for spelling edits only consider revisions that consist of one word, we do not consider revisions that are sentences
+    if len(deletion.split()) == 1 and len(addition.split()) ==1:
+
+      #ignores capitalization, lower cases everyword
+      word = deletion.split()[0].lower()
+      changed_word = addition.split()[0].lower()
+
+      #checks deletion word for corrections, uses the spell checker above to formulate a list of possible corrections
+      #if the addition word is in the correction list formulated by the spell checker, it is classified as a spelling correction
+
+      edits_list = known_edits2(word)
+      if changed_word in edits_list:
+        print ' SPELLING CORRECTION: ' + word + ' ... ' + changed_word 
+        if word == changed_word:
+          # if the addition and the deletion are the same we know that a capitalization correction was made
+          capital_correction += 1
+  
+
+        counter +=1
+
+print 'Total spelling corrections:',counter
+print 'Subtotal capitalization corrections:', capital_correction
+
+
+
+
+
--- Project/tools/feat_extract_task2.py
+++ Project/tools/feat_extract_task2.py
@@ -10,8 +10,8 @@ import re
 # this script extract desired features from the raw data set
 # and converts them to WEKA readable .arff format
 # input:  data directory
-#         desired save location+name
-# output: one .arff file       
+# output: text file with edit pairs 
+       
 
 # main function
 def main ():
@@ -19,185 +19,46 @@ def main ():
     # directory containing the raw .html files
     raw_data_dir = os.path.abspath(os.path.normpath(sys.argv[1]))
     print raw_data_dir
-    # directory where the results will be saved
-    # arff_dir = os.path.dirname(os.path.abspath(os.path.normpath(sys.argv[2])))
-    # arff_name = os.path.basename(os.path.abspath(os.path.normpath(sys.argv[2])))
-    # hard coded practice arff file - not sure what you wanted here???
-    # arff_name = 'practice_arff_file.arff'
 
-    # create the arff file
-    # try:
-    #     arff_file = open(arff_dir + '/' + arff_name, 'w')
-    #     print "created file: ", arff_dir + '/' + arff_name
-    # except:
-    #     sys.exit("failed to create arff file, exiting")
+    edits_file = open('../data/pair_edits.txt', 'w')
 
-    # # add headers to arff file
-    # arff_file.write("% 1. Title: StackExchange database\n")
-    # # arff.write("%% 1. METACRITIC %s database\n" % (arff_filename))
-
-    # arff_file.write("% \n")
-    # arff_file.write("%% 2. Created on %s \n" % (datetime.date.strftime(datetime.date.today(), "%Y-%M-%d")) )
-
-    # arff_file.write("@RELATION StackExchange\n\n")
-
-    # when adding a new feature, follow the numbered steps
-    # step 1 - add attribute name (ORDER MATTERS!)
-    # The <datatype> can be any of the four types currently (version 3.2.1) supported by Weka:
-    # numeric
-    # <nominal-specification>
-    # string
-    # date [<date-format>]
-    # more at: http://www.cs.waikato.ac.nz/ml/weka/arff.html
-
-    # feature 1: name
-    # arff_file.write("@ATTRIBUTE name string\n")
-
-    # # write data line
-    # arff_file.write("\n@DATA\n")
-
-    
 
     # walk the directory for data files
+    counter = 1
     for (path, dirs, files) in os.walk(raw_data_dir):
         for f in files:
             # work on .html files that are not revisions
-            if ".html" in f and "revision" not in f:
-
-                print "\n" + "working on " + path + '/' + f
-
-                # open the file
-                with open(path + '/' + f, 'r') as open_f:
-                    # make soup
-                    f_soup = BeautifulSoup(open_f)
-
-                # reset the values 
-                feat_values = []
-
-                # step 2 - extract the feature (KEEP SAME ORDER AS STEP 1)
-                # in most cases according to a function defined elsewhere
-                # only work on the above soup object (it's the main argument of your function)
-                # keep the work done here to a minimum
-                # and add it to feat_values
-                
-                # feature 1: name
-                feat = f
-                feat_values.append(feat)
-                print feat
-
-                # print extract_title(f_soup) + '\n'
-                # print extract_question(f_soup) + '\n'
-                # print extract_answer(f_soup) + '\n'
-
-            else:
+   
+            if "revision" in f:
                 with open(path + '/' + f, 'r') as open_f:
                     f_soup =  BeautifulSoup(open_f)
 
                 
-                extract_revision(f_soup)
+                    
+                    revisions = f_soup.find_all("div", {"class" : "post-text inline-diff"})
+                    for r in revisions:
+                        revs = r.find_all("span", {"class" : re.compile("diff-")})
+                        if len(revs) > 1:
+                            for i in revs[:-1]:
+                                index = revs.index(i)
+                                following_i = revs[index + 1]
 
-                # write the values to the file, not including answer&question media
-                # for v in feat_values[:-3]:
-
-                #     if type(v) == unicode:
-                #         # if the value is a string (ascii or unicode), add quotation marks for WEKA
-                #         arff_file.write('\'' + v.encode('ascii', 'ignore') + '\',')
-                #     elif type(v) == str:
-                #         arff_file.write('\'' + v + '\',')
-                #     else:
-                #         # if it's not a string (int etc.), write it as is
-                #         arff_file.write(str(v) + ',')
-
-                # # write categories to file without quotes
-                # arff_file.write(feat_values[-3] + ',')
-                # arff_file.write(feat_values[-2] + ',')
-
-                # # last value has linebreak, not comma
-                # if type(feat_values[-1]) == unicode:
-                #     arff_file.write('\'' + feat_values[-1].encode('ascii', 'ignore') + '\'\n')
-                # else:
-                #     arff_file.write(str(feat_values[-1]) + '\n')
+                                if i['class']  == ['diff-delete'] and following_i['class'] == ['diff-add']:
+                                    delete = i.getText().encode('ascii', 'ignore')
+                                    add = following_i.getText().encode('ascii', 'ignore')
+                                    print 'edit pair', counter
+                                    print 'writing to file, deletion' + delete
+                                    print 'writing to file, addition' + add
+                                    
+                                    edits_file.write('DELETION ' + str(counter) +': ' + delete + '\n')
+                                    edits_file.write('ADDITION ' + str(counter) +': ' + add + '\n')
+                                    counter += 1
 
 
-
-                
-    # close arff file
-    # arff_file.close()
-
-    # return nothing
     return None
 
 
-# def extract_tags(f_soup):
-#     # finds tags
-#     tag_list = []
-#     tags = f_soup.find_all("div", {"class" : "post-taglist"})
-#     for tag in tags:
-#         tag_list.append(tag.getText().strip())
 
-#     text = ' '.join(tag_list)
-#     # returns list of tags as 1 string
-
-#     # remove non-alphanumeric char
-#     text = re.sub(r'[^a-zA-Z0-9]+', ' ', text)
-#     return text
-
-def extract_title(f_soup):
-    # finds title (aka question title)
-    title = f_soup.find_all("div", {"id" : "question-header"})[0].getText().strip()
-    text = title
-
-    text = re.sub(r'[^a-zA-Z0-9]+', ' ', text)
-    return text
-
-
-def extract_question(f_soup):
-    # finds question 
-    question = f_soup.find_all("div", {"class" : "post-text"})[0].getText().strip()
-    question_cleaned = ' '.join(question.split())
-
-    text = question_cleaned
-    text = re.sub(r'[^a-zA-Z0-9]+', ' ', text)
-    return text
-
-
-def extract_answer(f_soup):
-    # finds answers
-    answers = f_soup.find_all("div", {"class" : "post-text"})[1:]
-    ans_list = []
-    for a in answers:
-        ans = a.getText().strip()
-        ans_cleaned = ' '.join(ans.split())
-        ans_list.append(ans_cleaned)
-    answers_string = ' '.join(ans_list)
-
-    text = answers_string
-    text = re.sub(r'[^a-zA-Z0-9]+', ' ', text)
-    return text
-
-def extract_revision(f_soup):
-  
-    revisions = f_soup.find_all("div", {"class" : "post-text inline-diff"})
-    for r in revisions:
-        # print r
-        revs = r.find_all("span", {"class" : re.compile("diff-")})
-        for i in revs:
-            print i
-        # deletions = r.find("span", {"class" : "diff-delete"})
-        # additions = r.find("span", {"class" : "diff-add"})
-        # try:
-        #     print 'DELETION: ' + deletions.getText()
-        #     print 'ADDITION: ' + additions.getText()
-        # except:
-        #     print 'NONE'
-        # deletions = r.find_all("span", {"class" : "diff-delete"})
-        # additions = f_soup.find_all("span", {"class" : "diff-add"})
-        # print 'DELETIONS:'
-        # for d in deletions:
-        #     print d.getText().strip()
-        # print 'ADDITIONS'
-        # for a in additions:
-        #     print a.getText().strip()
 
 
 # Call to main 
